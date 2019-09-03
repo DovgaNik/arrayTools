553d43b429f29862adb6f0288c43db38a3e7f9fc
DovgaNik <nikita.dovgan.05@hotmail.com>
2019-09-03 23:04:01 +0300

    Added function for removing item from float array

--- src/arrayTools/deleteItem.java
+++ src/arrayTools/deleteItem.java
@@ -38,4 +38,20 @@ public class deleteItem {
         
         return tempArr; //Returning temporary array
     }
+    float[] removeElement(float[] arr, int index){ //Function that deletes item from array by index (returns array with deleted item)
+        if (arr == null || index < 0 || index >= arr.length) { //Check if the array and the index are valid for deleting
+            return arr; 
+        }
+        
+        float[] tempArr = new float[arr.length - 1];  //Create new temporary array
+        
+        for (int i = 0, k = 0; i < arr.length; i++) { //Copying items to the temporary array
+            if (i == index) {
+                continue; 
+            } 
+            tempArr[k++] = arr[i]; 
+        }
+        
+        return tempArr; //Returning temporary array
+    }
 }
